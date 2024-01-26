e34cbef553c33eee3ba4a5a288f403628fde4a46
SumitKmrSingh <122218157+SumitKmrSingh@users.noreply.github.com>
2024-01-27 01:33:18 +0530

    secondCode

---
+++ AB.java
@@ -0,0 +1,13 @@
+public class AB
+{
+	void show(){
+		 System.out.println("Hello Show");
+	}
+
+	 public static void main(String[] args)
+	{
+	     AB obj =  new AB();
+	      obj.show();
+             System.out.println("Hello Java");
+	}
+}
